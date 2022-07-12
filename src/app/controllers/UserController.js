6e50d24f6a076f60120716d5a6df0f19187ed4b4
Wallace <wallacycleyton@gmail.com>
2022-07-12 13:38:09 -0300

    Adicionando funcionalidade de validacao de dados

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,22 @@
+import * as Yup from "yup";
 import Database from "../../database";
 
 const { User } = Database.connection.models;
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: "Falha na validação",
+      });
+    }
+
     const userExists = await User.findOne({
       where: {
         email: req.body.email,
@@ -44,6 +57,27 @@ class UserController {
       if (userExists) {
         return res.status(400).json({ error: "Usuário já cadastrado" });
       }
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .required()
+        .min(6)
+        .when("oldPassword", (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when("password", (password, field) =>
+        password ? field.required().oneOf([Yup.ref("password")]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: "Falha na validação",
+      });
     }
 
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
