1638e7469519a39ad6a93ea6b739d930a158ff3a
Wallace <wallacycleyton@gmail.com>
2022-07-12 15:17:55 -0300

    Adicionando validacao ao session controller

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,12 +1,26 @@
 import Database from "../../database";
 import jwt from "jsonwebtoken";
 import authConfig from "../../config/auth";
+import * as Yup from 'yup';
 
 const { User } = Database.connection.models;
 
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
+
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: "Falha na validação",
+      });
+    }
+
+
     const user = await User.findOne({
       where: {
         email,
